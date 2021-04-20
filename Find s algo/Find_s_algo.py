23d4bb8fd8a9b38990555006e2c729d39de51fd7
saihithesh123 <54038200+saihithesh123@users.noreply.github.com>
2021-04-20 15:54:49 +0530

    Update Find_s_algo.py

--- Find s algo/Find_s_algo.py
+++ Find s algo/Find_s_algo.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
  
 
-data = pd.read_csv("D:\NOTES\8th sem\ML Lab\Finds.cvs")
+data = pd.read_csv("D:\NOTES\6th sem\ML Lab\Finds.cvs")
 print(data,"\n")
  
 #array of all the attributes
